f9c3a7b9a8f8f5559ecc2bfaa2ca1c7df020ddf3
François Chalifour <francois.chalifour@gmail.com>
2019-11-05 10:38:16 +0100

    Comment how we compute the completion

--- src/Autocomplete.tsx
+++ src/Autocomplete.tsx
@@ -305,12 +305,21 @@ export function Autocomplete(props: AutocompleteProps) {
       state: getState(),
     });
 
+    // The completion should appear only if the _first_ characters of the query
+    // match with the suggestion.
     if (
       query &&
       currentSuggestion
         .toLocaleLowerCase()
         .indexOf(query.toLocaleLowerCase()) === 0
     ) {
+      // If the query typed has a different case than the suggestion, we want
+      // to show the completion matching the case of the query. This makes both
+      // strings overlap correctly.
+      // Example:
+      //  - query: 'Gui'
+      //  - suggestion: 'guitar'
+      //  => completion: 'Guitar'
       return query + currentSuggestion.slice(query.length);
     }
 

ba7fae40e236c9ab94f9ccc04c945568f45479a1
François Chalifour <francois.chalifour@gmail.com>
2019-11-08 18:48:38 +0100

    fix(SearchBox): convert SVG attributes to props

--- src/SearchBox.tsx
+++ src/SearchBox.tsx
@@ -39,27 +39,27 @@ export function SearchBox(props: SearchBoxProps) {
       onSubmit={props.onSubmit}
     >
       <label
-        for={props.getInputProps().id}
+        htmlFor={props.getInputProps().id}
         className="algolia-autocomplete-magnifierLabel"
       >
         <svg viewBox="0 0 18 18">
           <path
             d="M13.14 13.14L17 17l-3.86-3.86A7.11 7.11 0 1 1 3.08 3.08a7.11 7.11 0 0 1 10.06 10.06z"
             stroke="currentColor"
-            stroke-width="1.78"
+            strokeWidth="1.78"
             fill="none"
-            fill-rule="evenodd"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            fillRule="evenodd"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       </label>
 
       <div className="algolia-autocomplete-loadingIndicator">
-        <svg viewBox="0 0 38 38" stroke="currentColor" stroke-opacity=".5">
-          <g fill="none" fill-rule="evenodd">
-            <g transform="translate(1 1)" stroke-width="2">
-              <circle stroke-opacity=".3" cx="18" cy="18" r="18" />
+        <svg viewBox="0 0 38 38" stroke="currentColor" strokeOpacity=".5">
+          <g fill="none" fillRule="evenodd">
+            <g transform="translate(1 1)" strokeWidth="2">
+              <circle strokeOpacity=".3" cx="18" cy="18" r="18" />
               <path d="M36 18c0-9.94-8.06-18-18-18">
                 {/*
                 // @ts-ignore */}
@@ -132,7 +132,7 @@ export function SearchBox(props: SearchBoxProps) {
           <path
             d="M5 4.12L8.93.18a.62.62 0 1 1 .89.89L5.88 5l3.94 3.93a.62.62 0 1 1-.89.89L5 5.88 1.07 9.82a.62.62 0 1 1-.89-.89L4.12 5 .18 1.07a.62.62 0 1 1 .89-.89L5 4.12z"
             fill="currentColor"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
         </svg>
       </button>

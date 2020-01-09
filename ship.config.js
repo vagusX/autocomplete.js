6885104d75be433fba27d709ff37bebbc7b20e3c
François Chalifour <francois.chalifour@gmail.com>
2020-01-09 11:27:49 +0100

    chore(release): comment out example upgrade for now

--- ship.config.js
+++ ship.config.js
@@ -15,19 +15,21 @@ module.exports = {
     const versionPath = path.resolve(dir, 'src/version.ts');
     fs.writeFileSync(versionPath, `export const version = '${version}';\n`);
   },
-  afterPublish({ exec, dir }) {
-    // Update the Autocomplete.js version in the examples
-    const examplePath = path.resolve(dir, 'examples/autocomplete.js');
-    const { version } = require('./package.json');
+  // @TODO: this will be possible once this is addressed:
+  // https://github.com/algolia/shipjs/issues/604
+  // afterPublish({ exec, dir }) {
+  //   // Update the Autocomplete.js version in the examples
+  //   const examplePath = path.resolve(dir, 'examples/autocomplete.js');
+  //   const { version } = require('./package.json');
 
-    // eslint-disable-next-line no-console
-    console.log('Updating Autocomplete.js dependency in examples...');
+  //   // eslint-disable-next-line no-console
+  //   console.log('Updating Autocomplete.js dependency in examples...');
 
-    exec(
-      `cd ${examplePath} && yarn upgrade @francoischalifour/autocomplete.js@${version}`
-    );
-    exec('git add examples');
-    exec('git commit -m "chore(examples): update autocomplete.js dependency"');
-    exec('git push origin next');
-  },
+  //   exec(
+  //     `cd ${examplePath} && yarn upgrade @francoischalifour/autocomplete.js@${version}`
+  //   );
+  //   exec('git add examples');
+  //   exec('git commit -m "chore(examples): update autocomplete.js dependency"');
+  //   exec('git push origin next');
+  // },
 };
